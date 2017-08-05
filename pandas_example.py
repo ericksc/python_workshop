b991d04fd801fcc8b04cd064db0ed00636ed5f3e
ericksc <ericksc@gmail.com>
2017-08-05 16:12:23 -0600

    second working pandas example

--- pandas_example.py
+++ pandas_example.py
@@ -8,6 +8,18 @@ list_dict = [{'points': 50, 'time': '5:00', 'year': 2010},
 def list_of_dict_to_dataframe(df):
     return pd.DataFrame(df)
 
+def get_list_of_columns_on_dataframe(df):
+    return list(df.columns.values)
+
 df = list_of_dict_to_dataframe(list_dict)
+
+my_list = get_list_of_columns_on_dataframe(df)
+
+my_list.reverse()
+
+sub_list = ['year', 'time', 'points_h1', 'points']
+
+my_df = df[my_list]
+sub_my_df = df[sub_list]
 
 print('end')
