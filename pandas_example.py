b05ec00415e573c1e5ed2e1b57e189cbf397d9b5
ericksc <ericksc@gmail.com>
2017-08-05 16:33:57 -0600

    second working pandas example

--- pandas_example.py
+++ pandas_example.py
@@ -5,11 +5,33 @@ list_dict = [{'points': 50, 'time': '5:00', 'year': 2010},
              {'points':90, 'time': '9:00', 'month': 'january'},
              {'points_h1':20, 'month': 'june'}]
 
+def remove_element_on_list(element, object_list):
+    if element in object_list:
+        object_list.remove(element)
+
+def remove_element_of_list(element, object_list):
+    copied_list = object_list.copy()
+    if element in copied_list:
+        copied_list.remove(element)
+    return copied_list
+
+
 def list_of_dict_to_dataframe(df):
     return pd.DataFrame(df)
 
 def get_list_of_columns_on_dataframe(df):
     return list(df.columns.values)
+
+def delete_column_of_dataframe(df, column_name):
+    return df.drop(column_name, 1)
+
+
+
+def delete_column_on_dataframe(df, column_name):
+    df.drop(column_name, axis=1, inplace=True)
+
+def get_sub_dataframe(df, my_list):
+    return df[my_list]
 
 df = list_of_dict_to_dataframe(list_dict)
 
@@ -17,9 +39,13 @@ my_list = get_list_of_columns_on_dataframe(df)
 
 my_list.reverse()
 
-sub_list = ['year', 'time', 'points_h1', 'points']
-
 my_df = df[my_list]
-sub_my_df = df[sub_list]
+remove_element_on_list('year', my_list)
+
+
+sub_my_df = get_sub_dataframe(df, my_list)
+
+
+
 
 print('end')
